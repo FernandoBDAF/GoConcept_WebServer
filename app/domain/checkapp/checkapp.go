e9778f6d5f282517f4bf6669b0d3ab1255fc6ab1
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2025-03-06 16:16:33 -0800

    commit 15: implement middleware support

--- app/domain/checkapp/checkapp.go
+++ app/domain/checkapp/checkapp.go
@@ -4,8 +4,9 @@ package checkapp
 import (
 	// "context"
 	"context"
-	"encoding/json"
 	"net/http"
+
+	"github.com/fernandobdaf/GoConcept_WebServer/foundation/web"
 	// "github.com/fernandobdaf/GoConcept_WebServer/app/sdk/errs"
 	// "github.com/fernandobdaf/GoConcept_WebServer/business/sdk/sqldb"
 	// "github.com/fernandobdaf/GoConcept_WebServer/foundation/logger"
@@ -31,7 +32,7 @@ func newApp() *app {
 // readiness checks if the database is ready and if not will return a 500 status.
 // Do not respond by just returning an error because further up in the call
 // stack it will interpret that as a non-trusted error.
-func (a *app) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+func (a *app) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) web.MockEncoder {
 	// ctx, cancel := context.WithTimeout(ctx, time.Second)
 	// defer cancel()
 
@@ -46,14 +47,22 @@ func (a *app) readiness(ctx context.Context, w http.ResponseWriter, r *http.Requ
 	}{
 		Status: "OK",
 	}
-	return json.NewEncoder(w).Encode(status)
+	// return json.NewEncoder(w).Encode(status)
+
+	encoder := web.MockEncoder{
+		Data:        status,
+		ContentType: "application/json",
+		Err:         nil,
+	}
+
+	return encoder
 }
 
 // liveness returns simple status info if the service is alive. If the
 // app is deployed to a Kubernetes cluster, it will also return pod, node, and
 // namespace details via the Downward API. The Kubernetes environment variables
 // need to be set within your Pod/Deployment manifest.
-func (a *app) liveness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+func (a *app) liveness(ctx context.Context, w http.ResponseWriter, r *http.Request) web.MockEncoder {
 	// host, err := os.Hostname()
 	// if err != nil {
 	// 	host = "unavailable"
@@ -78,5 +87,11 @@ func (a *app) liveness(ctx context.Context, w http.ResponseWriter, r *http.Reque
 	}{
 		Status: "OK",
 	}
-	return json.NewEncoder(w).Encode(status)
+
+	encoder := web.MockEncoder{
+		Data:        status,
+		ContentType: "application/json",
+		Err:         nil,
+	}
+	return encoder
 }
--- foundation/web/web.go
+++ foundation/web/web.go
@@ -3,14 +3,36 @@ package web
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
 
+// Encoder defines behavior that can encode a data model and provide
+// the content type for that encoding.
+type Encoder interface {
+	Encode() (data []byte, contentType string, err error)
+}
+
+type MockEncoder struct {
+	Data        any
+	ContentType string
+	Err         error
+}
+
+func (e *MockEncoder) Encode() (data []byte, contentType string, err error) {
+	data, err = json.Marshal(e.Data)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return data, e.ContentType, e.Err
+}
+
 // HandlerFunc represents a function that handles a http request within our own
 // little mini framework.
-type HandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+type HandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request) MockEncoder
 
 // App is the entrypoint into our application and what configures our context
 // object for each of our http handlers. Feel free to add any configuration
@@ -18,27 +40,31 @@ type HandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Reques
 type App struct {
 	*http.ServeMux
 	shutdown chan os.Signal
+	mw       []MidFunc
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...MidFunc) *App {
 	return &App{
 		ServeMux: http.NewServeMux(),
 		shutdown: shutdown,
+		mw:       mw,
 	}
 }
 
 // HandlerFunc sets a handler function for a given HTTP method and path pair
 // to the application server mux.
-func (a *App) HandlerFunc(pattern string, handlerFunc HandlerFunc) {
+func (a *App) HandlerFunc(pattern string, handlerFunc HandlerFunc, mw ...MidFunc) {
+	handlerFunc = wrapMiddleware(mw, handlerFunc)
+	handlerFunc = wrapMiddleware(a.mw, handlerFunc)
 
 	h := func(w http.ResponseWriter, r *http.Request) {
 
 		// PUT ANY CODE WE WANT TO RUN BEFORE THE HANDLER HERE
 
-		if err := handlerFunc(r.Context(), w, r); err != nil {
+		if encode := handlerFunc(r.Context(), w, r); encode.Err != nil {
 			// ERROR HANDLING HERE
-			fmt.Println(err)
+			fmt.Println(encode.Err)
 			return
 		}
 
